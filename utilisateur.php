9e5636cae8c33fa4ec217832343539a2d81a6c99
Maxime BEERNAERT <maximebeernaert@gmail.com>
2023-02-06 15:24:46 +0100

    testcommit

---
+++ utilisateur.php
@@ -0,0 +1,8 @@
+<?php
+// Vérifier que le nom d'utilisateur et le mdp entré en login correspondent dans la liste de la base de donnée 
+// Ouvrir la page correspondante au rôle d'utilisateur 
+
+
+
+
+?>
