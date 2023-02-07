b8cc33a55a67bb7ab57c416d7e84c5114c5c70fe
Moustique-77 <enzo077@outlook.fr>
2023-02-07 12:12:56 +0100

    enzo

--- PHP HTML/DBconnexion.php
+++ PHP HTML/DBconnexion.php
@@ -4,10 +4,6 @@ $unmae= "root";
 $password = "";
 $db_name = "quizzeo";
 $conn = mysqli_connect($sname, $unmae, $password, $db_name);
-
-echo $conn;
-echo "hello";
-
 if (!$conn) {
     echo "Connection failed! : " . mysqli_connect_error();
 }else {echo "Connecté!";}
