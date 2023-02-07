a84ccb909a40a85a07b93743b872350ef932c7fd
Maxime BEERNAERT <maximebeernaert@gmail.com>
2023-02-07 16:41:01 +0100

    maxime

---
+++ PHP HTML/usermenu.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Menu</title>
+</head>
+<body>
+<?php
+    require('DBconnexion.php');
+    echo "Vous êtes connecté sous le compte de ... (rentrer ici les infos du compte)";
+?>
