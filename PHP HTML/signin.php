7bc516044363c9681fc4faf4ac07ded2c92298ee
Moustique-77 <enzo077@outlook.fr>
2023-02-07 15:56:02 +0100

    enzo

--- PHP HTML/signin.php
+++ PHP HTML/signin.php
@@ -9,15 +9,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $mdp = $_POST['mdp'];
     $category = $_POST['category'];
 
+    extract($_POST);
+
     $sql = "INSERT INTO utilisateurs (nom_utilisateur, prenom_utilisateur, mail_utilisateur, mdp_utilisateur, type_utilisateur)
     VALUES ('$nom', '$prenom', '$email', '$mdp', '$category')";
+   
+   $sql->excute (array(
+        'nom_utilisateur' => $nom,
+        'prenom_utilisateur' => $prenom,
+        'mail_utilisateur' => $email,
+        'mdp_utilisateur' => $mdp,
+        'type_utilisateur' => $category
+    ));
 
     if (mysqli_query($conn, $sql)) {
         echo "Compte créé avec succès";
+
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
 
     mysqli_close($conn);
 }
+header("Location: login.html");
 ?>
