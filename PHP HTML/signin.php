3ac46bcbe2d4d819a38a8a89f1fe0573dd5cb963
Moustique-77 <enzo077@outlook.fr>
2023-02-09 13:31:28 +0100

    enzo

--- PHP HTML/signin.php
+++ PHP HTML/signin.php
@@ -57,23 +57,18 @@
                 }
             }
 
-        } else {
+        }
     ?>
 
 <form class="form" action="" method="post">
     <h1 class="login-title">Registration</h1>
     <input type="text" class="login-input" name="username" placeholder="Username" required />
     <input type="text" class="login-input" name="name" placeholder="Name" required />
-    <input type="text" class="login-input" name="email" placeholder="Email Adress">
-    <input type="password" class="login-input" name="password" placeholder="Password">
-    <input type="password" class="login-input" name="confirmPassword" placeholder="Confirm Password">
+    <input type="text" class="login-input" name="email" placeholder="Email Adress" require>
+    <input type="password" class="login-input" name="password" placeholder="Password" require>
+    <input type="password" class="login-input" name="confirmPassword" placeholder="Confirm Password" require>
     <input type="submit" name="submit" value="Register" class="login-button">
     <p class="link"><a href="login.php">Click to Login</a></p>
 </form>
-
-<?php
-    }
-
-?>
 </body>
 </html>
