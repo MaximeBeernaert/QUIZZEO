178e4806d54939431f5f8cd3c497078d59ad71b5
Moustique-77 <enzo077@outlook.fr>
2023-02-09 11:27:58 +0100

    enzo

--- PHP HTML/admin.php
+++ PHP HTML/admin.php
@@ -25,7 +25,7 @@
                 $resultCheck = mysqli_num_rows($result);
 
                 while ($user = mysqli_fetch_assoc($result)) {
-                    echo "ID : " . $user['id_utilisateur'] . " Nom :" . $user['nom_utilisateur'] . " Prenom : " . $user['prenom_utilisateur'] . " email : " . $user['mail_utilisateur'];
+                    echo "\r ID : " . $user['id_utilisateur'] . " Nom :" . $user['nom_utilisateur'] . " Prenom : " . $user['prenom_utilisateur'] . " email : " . $user['mail_utilisateur'];
                 }
 
                 ?>
