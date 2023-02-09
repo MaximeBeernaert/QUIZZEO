dfac42c63135bd70356ed28384b25ac66f48d797
Moustique-77 <enzo077@outlook.fr>
2023-02-09 12:41:50 +0100

    enzo

--- PHP HTML/signin.php
+++ PHP HTML/signin.php
@@ -59,18 +59,20 @@
                 }
             }
 
-    } else {
-?>
-    <form class="form" action="" method="post">
-        <h1 class="login-title">Registration</h1>
-        <input type="text" class="login-input" name="username" placeholder="Username" required />
-        <input type="text" class="login-input" name="name" placeholder="Name" required />
-        <input type="text" class="login-input" name="email" placeholder="Email Adress">
-        <input type="password" class="login-input" name="password" placeholder="Password">
-        <input type="password" class="login-input" name="confirmPassword" placeholder="Confirm Password">
-        <input type="submit" name="submit" value="Register" class="login-button">
-        <p class="link"><a href="login.php">Click to Login</a></p>
-    </form>
+        } else {
+    ?>
+
+<form class="form" action="" method="post">
+    <h1 class="login-title">Registration</h1>
+    <input type="text" class="login-input" name="username" placeholder="Username" required />
+    <input type="text" class="login-input" name="name" placeholder="Name" required />
+    <input type="text" class="login-input" name="email" placeholder="Email Adress">
+    <input type="password" class="login-input" name="password" placeholder="Password">
+    <input type="password" class="login-input" name="confirmPassword" placeholder="Confirm Password">
+    <input type="submit" name="submit" value="Register" class="login-button">
+    <p class="link"><a href="login.php">Click to Login</a></p>
+</form>
+
 <?php
     }
 ?>
