945976d78895b30a708d3da18351a324f5cf7735
Moustique-77 <enzo077@outlook.fr>
2023-02-09 18:04:17 +0100

    e

--- PHP HTML/createquizz.js
+++ PHP HTML/createquizz.js
@@ -3,21 +3,19 @@ class CreateQuizz {
         this.addQuestion.addEventListener('click', this.addQuestionJS())
     }
 
+    //Create a new question and add it to the quizz
     addQuestion() {
 
-        // create a new div element
-        const newDiv = document.createElement("div");
 
-        // and give it some content
-        const newContent = document.createTextNode("Hi there and greetings!");
 
-        // add the text node to the newly created div
-        newDiv.appendChild(newContent);
 
-        // add the newly created element and its content into the DOM
-        const currentDiv = document.getElementById("div1");
-        document.body.insertBefore(newDiv, currentDiv);
+
+    }
+
+    //Create a new answers and add it to the question 
+    addAnswer() {
 
     }
 }
+
 let createQuizz = new CreateQuizz()
