ee6a64556c7e8c7eadd32cabc34d6b2d15db68f2
Maxime BEERNAERT <maximebeernaert@gmail.com>
2023-02-09 11:52:33 +0100

    maxime

--- PHP HTML/usermenu.php
+++ PHP HTML/usermenu.php
@@ -12,6 +12,8 @@
     $query    = "SELECT * FROM `utilisateurs` WHERE mail_utilisateur='$email'";
     $result = mysqli_query($conn, $query);
     $user = mysqli_fetch_assoc($result);
-
     echo "Vous êtes connecté sous le compte de " . $user['nom_utilisateur'] ." ". $user['prenom_utilisateur'];
+    if($user['id_utilisateur']!=0) {
+        echo "<br> Vous pouvez créer des quizz.";
+    }
 ?>
