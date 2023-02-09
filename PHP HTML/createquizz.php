ec1b9a83d72a5edf8a2aa1c85dddc8f8bb9538a4
Maxime BEERNAERT <maximebeernaert@gmail.com>
2023-02-09 12:33:21 +0100

    maxime

--- PHP HTML/createquizz.php
+++ PHP HTML/createquizz.php
@@ -1,8 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Create Quizz</title>
+</head>
+<body>
 <?php
-session_start();
-require('DBconnexion.php');
-$user = $_SESSION['user'];
-
-
-
+    require('DBconnexion.php');
+    // When form submitted, insert values into the database.
+    if (isset($_REQUEST['username'])) {
+        session_start();
+        require('DBconnexion.php');
+        $user = $_SESSION['user'];
+    } else {
 ?>
+    <form class="form" action="" method="post">
+        <h1 class="Create Quizz">Create Quizz</h1>
+        <input type="text" class="Quizz Title" name="quizztitle" placeholder="Titre Quizz" required />
+        <input type="submit" name="submit" value="Valider" class="submit-button">
+    </form>
+<?php
+    }
+?>
+</body>
+</html>
