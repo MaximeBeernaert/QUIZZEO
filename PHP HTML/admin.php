8a04bb97540227b5aac34bde68a16a0ac89499b1
Moustique-77 <enzo077@outlook.fr>
2023-02-10 10:27:21 +0100

    enzo

--- PHP HTML/admin.php
+++ PHP HTML/admin.php
@@ -16,69 +16,69 @@
         <h1>Interface administrateur</h1>
 
         <div class="users">
-            <div class="col-6">
-                <h2>Listes des utilisateurs de Quizzeo :</h2>
+            <h2>Listes des utilisateurs de Quizzeo :</h2>
 
-                <?php
+            <?php
 
-                require 'DBconnexion.php';
+            require 'DBconnexion.php';
 
-                $sql = "SELECT * FROM utilisateurs";
-                $result = mysqli_query($conn, $sql);
-                $resultCheck = mysqli_num_rows($result);
+            $sql = "SELECT * FROM utilisateurs";
+            $result = mysqli_query($conn, $sql);
+            $resultCheck = mysqli_num_rows($result);
 
-                // display all users in the database as a table whit their id, name, firstname, email and type and option button to delete them, edit them or add them
-                ?>
-                <table class='table table-striped'>
-                <thead>
-                    <tr>
-                        <th scope='col'>ID</th>
-                        <th scope='col'>Nom</th>
-                        <th scope='col'>Prenom</th>
-                        <th scope='col'>Email</th>
-                        <th scope='col'>Type</th>
-                        <th scope='col'>Action</th>
-                    </tr>
-                </thead>
-                <?php
-                while ($user = mysqli_fetch_assoc($result)) {
-                    // change the type of user from number to string to display it
-                    switch ($user['type_utilisateur']) {
-                        case 1:
-                            $user['type_utilisateur'] = "Quizzeur";
-                            break;
-                        case 2:
-                            $user['type_utilisateur'] = "Administrateur";
-                            break;
-                        default:
-                            $user['type_utilisateur'] = "Utilisateur";
-                            break;
+            // display all users in the database as a table whit their id, name, firstname, email and type and option button to delete them, edit them or add them
+            ?>
+            <table class='table table-striped'>
+            <thead>
+                <tr>
+                    <th scope='col'>ID</th>
+                    <th scope='col'>Nom</th>
+                    <th scope='col'>Prenom</th>
+                    <th scope='col'>Email</th>
+                    <th scope='col'>Type</th>
+                    <th scope='col'>Action</th>
+                 </tr>
+            </thead>
+                
+            <?php
+            while ($user = mysqli_fetch_assoc($result)) {
+                // change the type of user from number to string to display it
+                switch ($user['type_utilisateur']) {
+                    case 1:
+                        $user['type_utilisateur'] = "Quizzeur";
+                        break;
+                    case 2:
+                        $user['type_utilisateur'] = "Administrateur";
+                        break;
+                    default:
+                        $user['type_utilisateur'] = "Utilisateur";
+                        break;
                     }
 
-                    echo
-                    "<tbody>
-                        <tr>
+                echo
+                "<tbody>
+                    <br><tr>
 
-                            <td>" . $user['id_utilisateur'] . "</td>
-                            <td>" . $user['nom_utilisateur'] . "</td>
-                            <td>" . $user['prenom_utilisateur'] . "</td>
-                            <td>" . $user['mail_utilisateur'] . "</td>
-                            <td>" . $user['type_utilisateur'] . "</td>
+                        <td>" . $user['id_utilisateur'] . "</td>
+                        <td>" . $user['nom_utilisateur'] . "</td>
+                        <td>" . $user['prenom_utilisateur'] . "</td>
+                        <td>" . $user['mail_utilisateur'] . "</td>
+                        <td>" . $user['type_utilisateur'] . "</td>
 
-                            <td>
-                            <button type='button' class='btn-modif'>Modifier</button>
-                            <button type='button' class='btn-suppr'>Supprimer</button>
-                            </td>
+                        <td>
+                        <button type='button' class='btn-modif'>Modifier</button>
+                        <button type='button' class='btn-suppr'>Supprimer</button>
+                        </td>
 
-                        </tr>
-                    </tbody>
-                </table>";
-                }
-                ?>
-
-            </div>
+                    </tr>
+                </tbody>
+            </table>";
+            }
+            ?>
 
         </div>
+
+     </div>
 </body>
 
 </html>
