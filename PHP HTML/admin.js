c00842cc8773873cf093cea1952574276b78df50
Moustique-77 <enzo077@outlook.fr>
2023-02-10 12:07:37 +0100

    enzo

--- PHP HTML/admin.js
+++ PHP HTML/admin.js
@@ -9,11 +9,12 @@ class Admin {
     // Call admin controller to remove user from database and ask for confirmation
     removeUserJS() {
         // Get user id and name
-        let id = document.querySelector('.removeUser').getAttribute('data-id')
-        let name = document.querySelector('.removeUser').getAttribute('data-name')
+        let id = document.querySelector('.removeUser').getAttribute('id_utilisateur')
+        let nom = document.querySelector('.removeUser').getAttribute('nom_utilisateur')
+        let prenom = document.querySelector('.removeUser').getAttribute('prenom_utilisateur')
 
         // Ask for confirmation
-        if (confirm(`Voulez-vous vraiment supprimer l'utilisateur ${name} ?`)) {
+        if (confirm(`Voulez-vous vraiment supprimer l'utilisateur ${nom} ${prenom} ?`)) {
             fetch(`admin.php?id=${id}`, {
                 method: "DELETE"
             })
