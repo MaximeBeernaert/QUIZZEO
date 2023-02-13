c39ce9bb9928891a01ab12fb6f576cc671c45265
Moustique-77 <enzo077@outlook.fr>
2023-02-13 14:15:47 +0100

    enzo

--- PHP HTML/admin.php
+++ PHP HTML/admin.php
@@ -21,21 +21,22 @@
 
             <?php
 
-            if (mysqli_fetch_assoc == 0) {
-                echo "Il n'y a pas d'utilisateurs dans la base de données";
-            };
-
             require 'DBconnexion.php';
 
             $sql = "SELECT * FROM utilisateurs";
             $result = mysqli_query($conn, $sql);
             $resultCheck = mysqli_num_rows($result);
+            $checkIfNull = mysqli_fetch_assoc($result);
 
+            if ($checkIfNull == null) {
+                echo "<h4> Il n'y a pas d'utilisateurs dans la base de données </h4>";
+                return;
+            }
             // display all users in the database as a table whit their id, name, firstname, email and type and option button to delete them, edit them or add them
             ?>
-            </tbody>
+        </tbody>
             </table>
-
+    
             <table>
                 <thead>
                     <tr>
@@ -49,17 +50,16 @@
                     </tr>
                 </thead>
                 <tbody>
-                <?php while ($row = mysqli_fetch_assoc($result)): ?>
-                <?php
+                <?php while ($row = mysqli_fetch_assoc($result)):
                     // change the type of user from number to string to display it
                     switch ($row['type_utilisateur']) {
                         case 1:
                             $row['type_utilisateur'] = "Quizzeur";
-                            break;
+                                reak;
                         case 2:
                             $row['type_utilisateur'] = "Administrateur";
                             break;
-                         default:
+                        default:
                             $row['type_utilisateur'] = "Utilisateur";
                             break;
                     }
@@ -72,36 +72,35 @@
                         <td><?php echo $row['date_creation_utilisateur']; ?></td>
                         <td><?php echo $row['type_utilisateur']; ?></td>
                         <td>
-
+    
                         <form action="admin.php" method="POST">
                             <input type="hidden" name="id" value="<?php echo $row['id_utilisateur']; ?>">
                             <button type="submit" name="modify-btn" class="modify-btn">Modifier</button>
                         </form>
-                        
+                            
                         <form action="admin.php" method="POST">
                             <input type="hidden" name="id" value="<?php echo $row['id_utilisateur']; ?>">
                             <button type="submit" name="delete-btn" class="delete-btn">Supprimer</button>
                         </form>
-                    </td>
-                    </tr>
-                    <?php endwhile; ?>
-                </tbody>
-            </table>
 
+                    </tr>
+                <?php endwhile; ?>
+            </tbody>
+        </table>
+    
             <?php
-            // if the user click on the delete button, delete the user from the database
+            // if the user click on the delete button, delete the user from the database and ask confirmation
             if (isset($_POST['delete-btn'])) {
                 $id = $_POST['id'];
                 $sql = "DELETE FROM utilisateurs WHERE id_utilisateur = $id";
                 $result = mysqli_query($conn, $sql);
                 header("Location: admin.php");
             }
-
-
+    
             // if the user click on the modify button, redirect to the modify page
             //TODO: create the modify page
             ?>
-        
+
         </div>
 
     </div>
