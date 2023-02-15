cb6a2e09c139c420426c31e175b00e9ec88b6fbc
Moustique-77 <enzo077@outlook.fr>
2023-02-15 15:43:42 +0100

    enzo

--- PHP HTML/myquizz.php
+++ PHP HTML/myquizz.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta charset="utf-8"/>
-    <title>Gérer mes Quizz</title>
-    <title>Gérer mes Quizz</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
+
 <body>
+
+    <h1>My Quizz</h1>
+
     <table>
 <?php
     session_start();
@@ -21,6 +26,14 @@
     }
     
     $result = mysqli_query($conn, $query);
+    $resultCheck = mysqli_num_rows($result);
+    
+    //don't display if the quizz list if is empty
+    if ($resultCheck < 1) {
+        echo "Il n'y a pas de quizz dans la base de données";
+        return;
+    }
+
     while ($row = mysqli_fetch_assoc($result)):
         $id_user = $row['auteur_quizz'];
 ?>
@@ -29,24 +42,50 @@
             <td><?php echo $id_user; ?></td>
             <td><?php echo $row['date_creation_quizz']; ?></td>
             <td>
-                <form action="quizzlist.php" method="POST">
+                <form action="myquizz.php" method="POST">
                     <input type="hidden" name="id" value="<?php echo $row['titre_quizz']; ?>">
                     <button type="submit" name="modify-btn" class="modify-btn">Modifier</button>
                 </form>
             </td>
-            <td>                            
-                <form action="quizzlist.php" method="POST">
-                    <input type="hidden" name="id" value="<?php echo $row['titre_quizz']; ?>">
+            <td>    
+                <form action="myquizz.php" method="POST">
+                    <?php $id_quizz = $row['id_quizz'];
+                    echo "<input type='hidden' name='id' value=$id_quizz>"?>
                     <button type="submit" name="delete-btn" class="delete-btn">Supprimer</button>
-                </form>
+                </form>                       
+
             </td>
         </tr>
 <?php endwhile; ?>
     </table>
 <?php
+    // if the user click on the delete button, delete the quiz from the database and ask confirmation with a button
+    if (isset($_POST['delete-btn'])) {
+        $id = $_POST['id'];
+        echo "<p>Êtes-vous sûr de vouloir supprimer ce quizz ?</p>";
+        echo "<form action='myquizz.php' method='POST'>
+                <input type='hidden' name='id' value='$id'>
+                <button type='submit' name='confirm-delete-btn' class='confirm-delete-btn'>Oui</button>
+                <button type='submit' name='cancel-delete-btn' class='cancel-delete-btn'>Non</button>
+            </form>";
+    }
+    if (isset($_POST['confirm-delete-btn'])) {
+        $id = $_POST['id'];
+        $sql = "DELETE FROM quizz WHERE id_quizz = $id";
+        $result = mysqli_query($conn, $sql);
+        if ($result) {
+            echo "Quizz supprimé avec succès";
+            header("Location: myquizz.php");
+        }else{
+            echo "Erreur lors de la suppression du quizz";
+        }
+    }
 
-// DELETE BUTTON 
+
+
 // MODIFY BUTTON
 
 ?>
+
+</body>
 </html>
