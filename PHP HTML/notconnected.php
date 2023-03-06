bf8fd6e2f573962d1b1ecf9e478ffefd16cca26b
Maxime BEERNAERT <maximebeernaert@gmail.com>
2023-03-06 11:20:23 +0100

    maxime

---
+++ PHP HTML/notconnected.php
@@ -0,0 +1,4 @@
+<?php
+// When the user is not yet logged in, he is forwarded to this page, where he has the option to go to Login, or the Main Page. 
+echo "Vous n'êtes pas encore connecté !<br><p class='link'><a href='login.php'>login</a></p><br><p class='link'><a href='accueil.php'>accueil</a></p><br>"
+?>
