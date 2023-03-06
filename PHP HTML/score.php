82a01a5de6a60b1f08a945583997ef7f1161188f
Maxime BEERNAERT <maximebeernaert@gmail.com>
2023-03-06 11:12:05 +0100

    maxime

--- PHP HTML/score.php
+++ PHP HTML/score.php
@@ -15,6 +15,36 @@ $user = $_SESSION['user'];
 echo "<p class='link'><a href='usermenu.php'>Revenir au menu Utilisateur</a></p><br>";
 echo "<br>Votre Moyenne générale aux Quizz : ";
 
+$id_user = $user['id_utilisateur'];
+$quizz_total_average = [];
+$quizz_list = [];
+
+
+$query    = "SELECT * FROM `jouer` WHERE `id_utilisateur` = $id_user";
+$result = mysqli_query($conn, $query);
+while ($row_quizz_played = mysqli_fetch_assoc($result)) {
+    array_push($quizz_total_average, $row_quizz_played['score']);
+}
+$average_result = round(array_sum($quizz_total_average) / count($quizz_total_average), 2);
+echo $average_result;
+
+$query    = "SELECT * FROM `quizz`";
+$result = mysqli_query($conn, $query);
+while ($row_quizz_list = mysqli_fetch_assoc($result)) {
+    array_push($quizz_list, $row_quizz_list['id_quizz']);
+}
+
+for ($index=0; $index<count($quizz_list);$index++) {
+    $quizz_average = [];
+    $quizz = $quizz_list[$index];
+    $query    = "SELECT * FROM `jouer` WHERE `id_utilisateur` = $id_user AND `id_quizz` = $quizz";
+    $result = mysqli_query($conn, $query);
+    while ($row_quizz = mysqli_fetch_assoc($result)) {
+        array_push($quizz_average, $row_quizz['score']);
+    }
+    $average_result = round(array_sum($quizz_average) / count($quizz_average), 2);
+    echo "<br>Votre moyenne pour le quizz ". $quizz ." : ".$average_result;
+}
 
 
 ?>
