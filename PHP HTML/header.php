6b40be5e6d527cdf5661eb278041d527d0c2d833
Moustique-77 <enzo077@outlook.fr>
2023-03-07 12:26:15 +0100

    enzo

--- PHP HTML/header.php
+++ PHP HTML/header.php
@@ -12,13 +12,14 @@
     session_start();
     require('DBconnexion.php');
     $user = $_SESSION['user'];
+    $type_utilisateur = $user['type_utilisateur'];
     ?>
     <header>
         <div class="image">
             <img src="6221df85c9729_.png" class="image">
             <h1><a href="accueil.php">QUIZZEO</a></h1>
         </div>
-        <h2><a href="score.php">Scores</a> | <a href="admin.php">Panel admin</a> | <a href="createquizz.php">Créer un Quizz</a> | <a href="logout.php">Déconnexion</a></h2>
+        <h2><a href="score.php">Scores</a> <?php if ($type_utilisateur == 2) : ?> | <a href="admin.php">Panel admin</a><?php endif ?> | <a href="createquizz.php">Créer un Quizz</a> | <a href="logout.php">Déconnexion</a></h2>
     </header>
     <footer>
         <p> Company ©IPSSI . All rights reserved.</p>
