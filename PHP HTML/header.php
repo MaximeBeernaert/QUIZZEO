d72999b5840b6cc30259204547364d429ef6e2c9
Moustique-77 <enzo077@outlook.fr>
2023-03-08 10:58:19 +0100

    enzo

--- PHP HTML/header.php
+++ PHP HTML/header.php
@@ -15,14 +15,27 @@
     $type_utilisateur = $user['type_utilisateur'];
     ?>
     <header>
+
         <div class="image">
-            <img src="6221df85c9729_.png" class="image">
+            <img src="ipssi-logo.png" class="image">
+        </div>
+
+        <div class="quizzeo">
             <h1><a href="accueil.php">QUIZZEO</a></h1>
         </div>
-        <h2><a href="score.php">Scores</a> <?php if ($type_utilisateur == 2) : ?> | <a href="admin.php">Panel admin</a><?php endif ?><?php if ($type_utilisateur >= 1) : ?> | <a href="createquizz.php">Créer un Quizz</a><?php endif ?> | <a href="logout.php">Déconnexion</a></h2>
+
+        <div class="panel">
+            <h2><a href="score.php">Scores</a> <?php if ($type_utilisateur == 2) : ?> | <a href="admin.php">Panel admin</a><?php endif ?><?php if ($type_utilisateur >= 1) : ?> | <a href="createquizz.php">Créer un Quizz</a><?php endif ?> | <a href="logout.php">Déconnexion</a></h2>
+        </div>
+
+        <div class="userinfo">
+            <p>Vous êtes connecté sous le compte de <?php echo $user['nom_utilisateur'] . " " . $user['prenom_utilisateur'] ?></p>
+        </div>
+
     </header>
+
     <footer>
-        <p> Company ©IPSSI . All rights reserved.</p>
+        <p>Company ©MEL . All rights reserved.</p>
     </footer>
 </body>
 
