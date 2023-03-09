2bd3f716c2dea3db32f2175097d6898d483b5a3d
Moustique-77 <enzo077@outlook.fr>
2023-03-09 14:25:19 +0100

    enzo

--- PHP HTML/login.php
+++ PHP HTML/login.php
@@ -35,7 +35,7 @@
         } else {
             echo "<div class='form'>
                   <h3>Mot de passe ou identifiant incorrecte</h3><br/>
-                  <p class='link'>Cliquez ici pour <a href='login.php'></a>réessayer</p>
+                  <p class='link'>Cliquez ici pour <a href='accueil.php'></a>réessayer</p>
                   </div>";
         }
     } else {
