dcc2b85eae4b856dcc52c35d4d06bdd7d0c40a8d
Moustique-77 <enzo077@outlook.fr>
2023-03-09 12:40:55 +0100

    enzo

--- PHP HTML/notconnected.php
+++ PHP HTML/notconnected.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tu n'es pas connecter !</title>
 </head>
 
 <body>
@@ -14,19 +15,16 @@
     ?>
   </header>
 
-  <div class="notcoonected_container">
+  <!-- // When the user is not yet logged in, he is forwarded to this page, where he has the option to go to Login, or the Main Page.  -->
 
-    <?php
-    // When the user is not yet logged in, he is forwarded to this page, where he has the option to go to Login, or the Main Page. 
-    echo
-    "Vous n'êtes pas encore connecté !
-    <br>
-    <p class='link'><a href='login.php'>Connexion</a></p>
-    <br>
-    <p class='link'><a href='accueil.php'>Accueil</a></p><br>";
-    require('header.php');
-    ?>
-
+  <div class="containerNotConnected">
+    <div class="textNotConnected">
+      Vous n'êtes pas encore connecté !
+    </div>
+    <div class="btnNotConnected">
+      <a href="login.php">Connexion</a>
+      <a href="accueil.p">Accueil</a>
+    </div>
   </div>
 </body>
 
--- PHP HTML/notpermited.php
+++ PHP HTML/notpermited.php
@@ -1,4 +1,32 @@
-<?php
-// When the user is not correctly logged in, he is forwarded to this page, where he has the option to go to Login, or the Main Page. 
-echo "Vous n'avez pas les droits pour accéder à cette page !<br><p class='link'><a href='usermenu.php'>menu</a></p><br><p class='link'><a href='accueil.php'>accueil</a></p><br>"
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tu n'as pas la permission !</title>
+</head>
+
+<body>
+    <header>
+        <?php
+        require('header.php');
+        ?>
+    </header>
+
+    <!-- // When the user is not correctly logged in, he is forwarded to this page, where he has the option to go to Login, or the Main Page. -->
+
+    <div class="containerNotPermited">
+        <div class="textNotPermited">
+            Vous n'avez pas les droits pour accéder à cette page !
+        </div>
+        <div class="btnNotPermited">
+            <a href="usermenu.php">Menu utilisateur</a>
+            <a href="accueil.php">Accueil</a>
+        </div>
+    </div>
+
+</body>
+
+</html>
