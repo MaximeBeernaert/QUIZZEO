ac1398b01920012db48288f5e8448013137fa455
Léon <leonvachette@gmail.com>
2023-03-09 10:06:08 +0100

    leon

--- PHP HTML/header.php
+++ PHP HTML/header.php
@@ -12,18 +12,6 @@
     require('DBconnexion.php');
     $user = $_SESSION['user'];
  ?>
-        <header>
-            <div class="image">
-                <img src="image-removebg-preview (1).png" class="image">
-                <h1><a href="accueil.php">QUIZZEO</a></h1>
-            </div>
-            <h2><a href="">Scores</a> | <a href="">Panel admin</a> | <a href="">Questions quizz</a> | <a href="">Déconnexion</a></h2>
-        </header>
-        <div class="elements">
-        </div>
-        <footer>
-           <p> Company ©IPSSI . All rights reserved.</p>
-        </footer>
     <?php
     $type_utilisateur = $user['type_utilisateur'];
     ?>
