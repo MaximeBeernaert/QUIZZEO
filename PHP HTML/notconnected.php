151d8495b2ed651885f98534788afac7bf43235e
Moustique-77 <enzo077@outlook.fr>
2023-03-09 12:26:07 +0100

    enzo

--- PHP HTML/notconnected.php
+++ PHP HTML/notconnected.php
@@ -1,20 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<?php
-    require('header.php');
-  ?>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
 <body>
-<?php
-// When the user is not yet logged in, he is forwarded to this page, where he has the option to go to Login, or the Main Page. 
-echo "Vous n'êtes pas encore connecté !<br><p class='link'><a href='login.php'>login</a></p><br><p class='link'><a href='accueil.php'>accueil</a></p><br>";
- require('header.php');
-?>
+  <header>
+    <?php
+    require('header.php');
+    ?>
+  </header>
 
+  <div class="notcoonected_container">
+
+    <?php
+    // When the user is not yet logged in, he is forwarded to this page, where he has the option to go to Login, or the Main Page. 
+    echo
+    "Vous n'êtes pas encore connecté !
+    <br>
+    <p class='link'><a href='login.php'>Connexion</a></p>
+    <br>
+    <p class='link'><a href='accueil.php'>Accueil</a></p><br>";
+    require('header.php');
+    ?>
+
+  </div>
 </body>
-</html>
 
+</html>
