33cc238cab5da6d63ee1824471a01bc39d77b83b
Maxime BEERNAERT <maximebeernaert@gmail.com>
2023-03-10 13:13:15 +0100

    maxime

--- PHP HTML/header.js
+++ PHP HTML/header.js
@@ -13,28 +13,28 @@ class header {
         let currentHouse = houseButton.className.replace("houseButton ", "")
         switch(currentHouse) {
             case 'Griffondor':
+                options.style.backgroundColor = "#1c1c1c";
                 houseButton.className = "houseButton Poufsouffle"
                 header.style.backgroundImage = "url('poufsouffle.png')";
                 this.createCookie(`currentHouse`,`houseButton Griffondor`,"1")
-                options.style.backgroundColor = "#1c1c1c";
                 break;
             case 'Poufsouffle':
+                options.style.backgroundColor = "#000814";
                 houseButton.className = "houseButton Serdaigle"
                 header.style.backgroundImage = "url('serdaigle.png')";
                 this.createCookie(`currentHouse`,`houseButton Poufsouffle`,"1")
-                options.style.backgroundColor = "#000814";
                 break;
             case 'Serdaigle':
+                options.style.backgroundColor = "#134611";
                 houseButton.className = "houseButton Serpentard"
                 header.style.backgroundImage = "url('serpentard.png')";
                 this.createCookie(`currentHouse`,`houseButton Serdaigle`,"1")
-                options.style.backgroundColor = "#134611";
                 break;
             case 'Serpentard':
+                options.style.backgroundColor = "#220901";
                 houseButton.className = "houseButton Griffondor"
                 header.style.backgroundImage = "url('griffondor.png')";
                 this.createCookie(`currentHouse`,`houseButton Serpentard`,"1")
-                options.style.backgroundColor = "#220901";
                 break;
         }
         
