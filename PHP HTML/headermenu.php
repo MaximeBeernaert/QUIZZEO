0e6c40dc42b4ac73223aee407086701eeba0ac2d
Moustique-77 <enzo077@outlook.fr>
2023-03-11 11:43:00 +0100

    enzo

--- PHP HTML/headermenu.php
+++ PHP HTML/headermenu.php
@@ -12,21 +12,66 @@
     <?php
     session_start();
     require('DBconnexion.php');
-
+    if (!isset($_SESSION['user'])) {
     ?>
-    <header>
-        <div class="containerHeader">
-            <div class="containerIPSSI">
-                <img src="ipssi-logo.png" class="image">
-            </div>
-            <div class="containerQuizzeo">
-                <p><a href="accueil.php">QUIZZEO</a></p>
+        <header>
+            <div class="containerHeader">
+                <div class="containerPoudlard">
+                    <img src="poudlard.jpeg" class="image">
+                </div>
+                <div class="containerQuizzeo">
+                    <p><a href="accueil.php">QUIZZEO<br>HARRY POTTER</a></p>
 
+                </div>
+                <div class="containerAccount">
+                    <p class="accountName"><a href="login.php">Connexion</a></p>
+                    <p classe="decoText"><a href="signin.php">Inscription</a></p>
+                </div>
             </div>
-        </div>
-        <div class="containerOptions">
-            <p><a href="accueil.php">Retour à l'accueil</a></p>
-        </div>
 
-    </header>
-</body>
+        </header>
+    <?php
+    } else {
+    ?>
+        <header>
+            <div class="containerHeader">
+                <div class="containerIPSSI">
+                    <img src="ipssi-logo.png" class="image">
+                </div>
+                <div class="containerQuizzeo">
+                    <p><a href="accueil.php">QUIZZEO<br>HARRY POTTER</a></p>
+
+                </div>
+                <div class="containerAccount">
+                    <p class="accountName"><a href="personalspace.php">Compte de <?php echo $_SESSION['user']['prenom_utilisateur'] ?></a></p>
+                    <p classe="decoText"><a href="logout.php"><img class="decoIcone" src="deco.png" class="image"> Déconnexion</a></p>
+                </div>
+            </div>
+            <div class="containerOptions">
+                <?php
+                if ($_SESSION['user']['type_utilisateur'] == 2) {
+                    echo "<p class='optionHeader'><a href='admin.php'>Administration</a></p><p class='spaceHeader'>|</p>";
+                }
+                if ($_SESSION['user']['type_utilisateur'] != 0) {
+                    echo "<p class='optionHeader'><a href='createquizz.php'>Création de quizz</a></p> <p class='spaceHeader'>|</p>";
+                }
+                if (isset($_COOKIE['currentHouse'])) {
+                    $currentHouse = $_COOKIE['currentHouse'];
+                } else {
+                    $currentHouse = 'houseButton Serpentard';
+                }
+
+                ?>
+                <p class='optionHeader'><a href="score.php">Mes Scores</a></p>
+                <p class='spaceHeader'>|</p>
+                <p class='optionHeader'><a href="usermenu.php">Retour menu principal</a></p>
+                <p class='spaceHeader'>|</p>
+                <?php
+                echo "<img class='$currentHouse' src=''></div>"
+                ?>
+            </div>
+
+        </header>
+    <?php
+    }
+    ?>
