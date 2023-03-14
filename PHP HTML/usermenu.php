09c1f3eef924d9174d8fbf49a871cf0b9da49120
Maxime BEERNAERT <maximebeernaert@gmail.com>
2023-03-14 16:43:40 +0100

    maxime

--- PHP HTML/usermenu.php
+++ PHP HTML/usermenu.php
@@ -6,25 +6,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu utilisateur</title>
-    <style>
-
-    </style>
 </head>
 <script>
+    // Script to keep the scrolling position of the page when it is reloaded (by the carrousel of quiz)
     document.addEventListener("DOMContentLoaded", function(event) {
+        // check if localStorage.getItem('scrollpos') exist
         var scrollpos = localStorage.getItem('scrollpos');
         if (scrollpos) {
+            // if yes, the page moves to the scrolling position
             window.scrollTo(0, scrollpos);
         }
     });
-
     window.onbeforeunload = function(e) {
+        // on unload of page, we put the scroll position in the localStorage
         localStorage.setItem('scrollpos', window.scrollY);
     };
 </script>
 
 <body>
     <header>
+        <!-- we call for the header.php for the header HTML and the CSS -->
         <?php
         require('header.php');
         ?>
@@ -32,9 +33,11 @@
     <?php
     function quizzDivMaker($id_quizz,$conn)
     {
+        // Every times a quiz has to be created, we create it here. 
         if ($id_quizz == "<div></div>") {
             return $id_quizz;
         } else {
+            // We select it from the database, get its title, theme and difficulty
             $query = "SELECT * FROM `quizz` WHERE id_quizz='$id_quizz'";
             $result = mysqli_query($conn, $query);
             $quizz = mysqli_fetch_assoc($result);
@@ -42,6 +45,7 @@
             $theme_quizz = $quizz['theme_quizz'];
             $modif = '';
 
+            // with the difficulty, we put stars on the div card
             $star = "<i class='diffilculteStarMainQuizz'>&#9733;</i>";
             $diff = "<div class='starDiv'>";
             if ($quizz['difficulte_quizz'] == 1) {
@@ -60,6 +64,7 @@
             $user = $_SESSION['user'];
             $id_user = $user['id_utilisateur'];
             $query = "SELECT * FROM `quizz` WHERE id_quizz='$id_quizz' AND auteur_quizz='$id_user'";
+            // Next we check if the user is the owner or the admin of the site, so they can have access to Modify/Delete buttons
             $result = mysqli_query($conn, $query);
             if ($quizz = mysqli_fetch_assoc($result)) {
                 $modif = "<form action='modif.php' method='POST'>
@@ -82,6 +87,7 @@
             </form>";
             }
 
+            // we put everything in a template of divs and the quiz is created. 
 
             return "<div class='quizz'>
                 <div class='titlequizzmainpage'>
@@ -105,6 +111,7 @@
     }
     function checkSuppression()
     {
+        // we create a form if the user has clicked on a quiz suppression button
         if (isset($_POST['id_quizz'])) {
             $id_quizz = $_POST['id_quizz'];
             echo "
@@ -122,10 +129,12 @@
                         </div>
                         </div>
                         <br>";
+                        // if confirmed, it will launch the suppression()
         }
     }
     function suppression($conn)
     {
+        // here we suppress the quiz targeted with an sql query
         if (isset($_POST['confirm-delete-btn'])) {
 
             $id_quizz = $_POST['id_quizz'];
@@ -148,13 +157,18 @@
             $query = "DELETE FROM `quizz` WHERE id_quizz='$id_quizz'";
             $result = mysqli_query($conn, $query);
 
+            // and refresh the page
             echo ("<meta http-equiv='refresh' content='1'>");
         }
     }
+    // we create the carrousel here
     function carrouselMaker($carrouselMyQuizzArray, $get,$conn)
     {
         $previousLink = '';
         $quizzShown = $carrouselMyQuizzArray[1];
+        // depending on the arraysize, there will be one, two or three (and thus more) quiz shown on screen
+        // with each quiz id stored in the array, we create a 'quiz div' displaying the quiz infos.
+        // if the quiz array is longer than 2, we create two buttons on the side of the carrousel, so we can navigate throught the quiz.
         if (count($carrouselMyQuizzArray) == 2) {
             $quizzShown = quizzDivMaker($carrouselMyQuizzArray[1],$conn);
             return $quizzShown;
@@ -195,14 +209,16 @@
         }
     }
 
-
+// Then we call for the current colors (out of the SESSION cookies) chosen by the user (if not set, we put Serpentard>Griffondor colors)
     if (isset($_COOKIE['currentHouse'])) {
         $currentHouse = str_replace("houseButton ", "", $_COOKIE['currentHouse']);
     } else {
         $currentHouse = 'Serpentard';
     }
     ?>
+    <!-- Main page, where the banners and the carrousels of quiz will be -->
     <div class="mainPage">
+        <!-- first banner -->
         <div class="banner">
             <?php echo "<img class='houseIcone $currentHouse' src='GriffondorIcone.png'>" ?>
         </div>
@@ -216,15 +232,20 @@
             //if the user is a  utilisateur (type_utilisateur = 0), he can only see all the quizzes
             if ($type_utilisateur == 1 || $type_utilisateur == 2) :
             ?>
+            <!-- here we make the two carrousels -->
                 <div class="usermenuQuizzList">Mes quizz :</div>
             <?php
+    // first carrousel : 
                 $query = "SELECT * FROM `quizz` WHERE auteur_quizz='$id_utilisateur'";
                 $result = mysqli_query($conn, $query);
                 $carrouselMyQuizzArray = [0];
+                // we add each quiz that the user made in an array
                 while ($row = mysqli_fetch_assoc($result)) {
                     array_push($carrouselMyQuizzArray, $row['id_quizz']);
                 }
                 echo '<div class="carrouselMyQuizz">';
+                // if there is no quiz, 'you have no quiz'
+                // else, we make the carrousel with the quiz array
                 if (count($carrouselMyQuizzArray) == 1) {
                     echo "Tu n'as pas encore créé de quizz !";
                 } elseif (count($carrouselMyQuizzArray) >= 2) {
@@ -232,19 +253,25 @@
                 }
                 echo '</div>';
             endif;
+            // we check if the first delete button was pressed
             checkSuppression();
+            // we check if the second delete button was pressed (and delete the targeted quiz)
             suppression($conn);
             ?>
             <div class="usermenuQuizzList">Tous les quizz :</div>
             <?php
+    // second carrousel : 
             $query = "SELECT * FROM `quizz`";
             $result = mysqli_query($conn, $query);
             $carrouselMyQuizzArray = [0];
+            // we add each quiz existing in an array
             while ($row = mysqli_fetch_assoc($result)) {
                 array_push($carrouselMyQuizzArray, $row['id_quizz']);
             }
 
             echo '<div class="carrouselMyQuizz">';
+            // if there is no quiz, 'there is no quiz yet'
+            // else, we make the carrousel with the quiz array
             if (count($carrouselMyQuizzArray) == 1) {
                 echo "Il n'y a pas encore de quizz !";
             } else {
@@ -253,6 +280,7 @@
             echo '</div><br><br>';
             ?>
         </div>
+        <!-- second banner and footer -->
         <div class=" banner">
             <?php echo "<img class='houseIcone $currentHouse' src='GriffondorIcone.png'>" ?>
         </div>
